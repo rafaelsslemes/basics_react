97e015a47d3a339e1e3a517f1899d84d4410e252
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-09 17:18:33 -0300

    Comment example

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -1,7 +1,9 @@
 const SampleComponent = () => {
 
+    // React JSX always must be wrapped by an unique tag
     return (
-        <div>
+        <div> 
+            {/* Comment */}
             <h1>Sample Component</h1>
         </div>    
     );
