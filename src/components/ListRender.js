982c6d8d1a2abf79e7dc07574708e624ffffeb9d
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-12 20:51:49 -0300

    Change list by hooks

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -2,18 +2,29 @@ import { useState } from "react"
 
 const ListRender = () => {
 
-    const [list] = useState(['John','Peter', 'Mary']);
+    const [list, setList] = useState(['John', 'Peter', 'Mary']);
 
-  return (
-    <div>ListRender
-        <ul>    
-            {/* bad practice use list index, prefer unique ids generated */}
-            {list.map((item, i)=>(
-                <li key={i}>{item}</li>
-                ))}
-        </ul>
-    </div>
-  )
+    const addNumber = () => {
+        const pseudoRandomNum = Math.floor(Math.random() * 10);
+        
+        // The first param always is the previous value of variable hooked
+        setList(prevList => [...prevList, pseudoRandomNum]);
+            
+        console.log(list);
+    };
+    
+
+    return (
+        <div>ListRender
+            <ul>    
+                {/* bad practice use list index, prefer unique ids generated */}
+                {list.map((item, i)=>(
+                    <li key={i}>{item}</li>
+                    ))}
+            </ul>
+            <button onClick={addNumber}>Add Number</button>
+        </div>
+    )
 
 }
 
