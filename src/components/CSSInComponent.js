b5a8bc9e58dffd9a168a984e1f87715ffca7d30a
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-15 16:59:29 -0300

    CSS Inline

--- src/components/CSSInComponent.js
+++ src/components/CSSInComponent.js
@@ -6,8 +6,10 @@ const CSSInComponent = () => {
     <div>
         <h1> Inherited H1 Style</h1>
         <h1 className='comp-header'> Local H1 Style</h1>
+        <p style={{color:'pink', padding: '25px,', borderTop:'10px solid red',}}>CSS Inline Here</p>
     </div>
   )
+  
 }
 
 export default CSSInComponent
