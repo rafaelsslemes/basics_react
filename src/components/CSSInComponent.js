986de604ca0a34eea3656d3f8e75bfd2c6e4be18
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-16 07:43:47 -0300

    Dynamic Inline CSS

--- src/components/CSSInComponent.js
+++ src/components/CSSInComponent.js
@@ -2,11 +2,19 @@ import React from 'react'
 import "./CSSInComponent.css";
 
 const CSSInComponent = () => {
+
+const n = 12;
+
   return (
     <div>
         <h1> Inherited H1 Style</h1>
         <h1 className='comp-header'> Local H1 Style</h1>
-        <p style={{color:'pink', padding: '25px,', borderTop:'10px solid red',}}>CSS Inline Here</p>
+        <p style={{color:'pink', padding: '25px,', borderTop:'10px solid red',}}>
+            CSS Inline Here
+        </p>
+        <p style={n < 10 ? ({color:'purple'}) : ({color:'blue'})}>
+        Dynamic CSS Inline Here
+        </p>
     </div>
   )
   
