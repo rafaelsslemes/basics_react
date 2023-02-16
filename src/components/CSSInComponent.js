adf3c6155a0722973a8056afa89fdb3467321ed3
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-16 14:51:26 -0300

    Condictional CSS Classes

--- src/components/CSSInComponent.js
+++ src/components/CSSInComponent.js
@@ -4,16 +4,25 @@ import "./CSSInComponent.css";
 const CSSInComponent = () => {
 
 const n = 12;
+const name = 'Peter';
+const alert = false;
 
   return (
     <div>
         <h1> Inherited H1 Style</h1>
         <h1 className='comp-header'> Local H1 Style</h1>
-        <p style={{color:'pink', padding: '25px,', borderTop:'10px solid red',}}>
+        <p style={{color:'pink', padding: '25px', borderTop:'10px solid red',}}>
             CSS Inline Here
         </p>
         <p style={n < 10 ? {color:'purple'} : {color:'blue'}}>
         Dynamic CSS Inline Here
+        </p>
+        <p style={name === 'John' ? {color:'purple', padding: '10px'} : null}>
+        Dynamic CSS Inline Here - Null
+        </p>
+
+        <p className={alert ? 'red-title' : 'yellow-title'}>
+        Dynamic CSS Class Here
         </p>
     </div>
   )
