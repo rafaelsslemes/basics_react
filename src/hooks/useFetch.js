996e1a207c29a65f4bb3bea7759db8fe353433b8
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-22 12:31:41 -0300

    Receive a boolean to run fetch when necessary

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (url) => { 
+export const useFetch = (url, reload) => { 
     
     const [data, setData] = useState(null);
 
@@ -15,7 +15,7 @@ export const useFetch = (url) => {
 
         fetchData();
     
-    }, [url]);
+    }, [url, reload]);
 
     return {data};
 };
